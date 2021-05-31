7a91414940115bc41f5eec0d7676bb89d347d7e0
kcaldwell <kcaldwell@nuro.ai>
2021-05-31 16:02:39 -0700

    adding comments to base module

--- src/Modules/base_module.h
+++ src/Modules/base_module.h
@@ -8,14 +8,25 @@
 
 class BaseModule {
   public:
+    // Generic template for initialization, should return the module
+    // specific datatype. 
     virtual std::shared_ptr<ModuleData> Init();
+
+    // Generic function that calls Poll(), should be started by
+    // the process managing all of the modules. Since Loop() calls
+    // Poll(), we need to pass the variable containing all of the module
+    // information.
     void Loop(ModuleDataWrapper* data);
+
+    // TODO: kirencaldwell - should probably make a getter for this.
+    // Module name.
     std::string _module_name;
   protected:
+    // Generic function called by Loop(), this is where the meat of the
+    // module lives.
+    // Need to pass the variable containing all of the modules
+    // information.
     virtual void Poll(ModuleDataWrapper* data);
-    void CreateTimer(std::string timer_name);
-    
 };
-
 
 #endif
