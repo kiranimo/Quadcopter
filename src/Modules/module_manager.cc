8fdb83784fb01c49c0b3e6f6ebcef1e04b2a7a4a
kcaldwell <kcaldwell@nuro.ai>
2021-05-31 16:15:31 -0700

    cleaning up and adding comments to module_manager

--- src/Modules/module_manager.cc
+++ src/Modules/module_manager.cc
@@ -1,29 +1,30 @@
 #include "module_manager.h"
 
-ModuleManager::ModuleManager() {
-   
-}
-
 void ModuleManager::AddModule(std::unique_ptr<BaseModule> module) {
+  // transfers ownership of specific module to manager.
   _modules.push_back(std::move(module));
 }
 
 void ModuleManager::InitModules() {
+  // Initialize module and add module specific datatype to container.
   for (int i = 0; i < (int)_modules.size(); i++) {
     _module_data_wrapper.AddModuleData(_modules[i].get()->Init());
   } 
 }
 
 void ModuleManager::StartModules() {
-
   std::cout << "Starting threads" << std::endl;
+
+  // vector of threads
   std::vector<std::thread> module_threads;
   for (int i = 0; i < (int)_modules.size(); i++) {
     std::cout << "Starting " << _modules[i].get()->_module_name << std::endl;
+    // initialize threads
     module_threads.push_back(
         std::thread(&BaseModule::Loop, _modules[i].get(), &_module_data_wrapper));
   }
  
+  // join threads
   for (auto &th : module_threads) {
     th.join();
   }
--- src/Modules/module_manager.h
+++ src/Modules/module_manager.h
@@ -8,18 +8,29 @@
 #include "base_module.h"
 #include "module_data_wrapper.h"
 
+// Class responsible for managing the instatiation and running of
+// all derived modules.
 class ModuleManager {
 
   public:
-    ModuleManager();
+    // transfer ownership of module class to ModuleManager and
+    // add to container
     void AddModule(std::unique_ptr<BaseModule> module);
+
+    // Go through and initialize all of the modules. This also
+    // adds the module specific data to the shared wrapper.
     void InitModules();
+
+    // Create and start threads for each module, and have them
+    // start their Loop().
     void StartModules();
 
   private:
    
+    // Container for derived modules.
     std::vector<std::unique_ptr<BaseModule>> _modules;
 
+    // Container for all module specific data 
     ModuleDataWrapper _module_data_wrapper;
 };
 
