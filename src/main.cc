226db25cb9e771304ce818c2159bfbad3bad9538
kcaldwell <kcaldwell@nuro.ai>
2021-05-31 10:35:29 -0700

    correcting instatiation of TestModule

--- src/main.cc
+++ src/main.cc
@@ -4,39 +4,8 @@
 #include <memory>
 #include "Modules/base_module.h"
 #include "Modules/module_manager.h"
+#include "Modules/test_module.h"
 
-struct TestModuleDataType {
-  double dt;
-};
-
-class TestModule : public BaseModule {
-  public: 
-    TestModule(std::string module_name) {
-      _module_name = module_name;
-    }
-    void Init() override {
-    }
-  protected:
-    void Poll(ModuleDataWrapper* data) override {
-      std::string module_rw_name = "test_module1";
-
-      std::shared_ptr<ModuleData> module_data_container = 
-        std::make_shared<ModuleData>(module_rw_name);
-      ModuleData* md_ptr = data->GetModuleData(module_rw_name);
-      if (md_ptr != nullptr) {
-        _module_data.dt = md_ptr->get<TestModuleDataType>().dt;
-        _module_data.dt++;
-        std::cout << "Module: " <<  _module_name << ", dt = " << _module_data.dt << std::endl; 
-        module_data_container.get()->push(_module_data); 
-        data->SetModuleData(module_data_container, module_rw_name);
-      }
-      else {
-        std::cout << "nullptr" << std::endl;
-      }
-    } 
-
-    TestModuleDataType _module_data;
-};
 
 
 int main() {
@@ -51,8 +20,8 @@ int main() {
   std::shared_ptr<ModuleData> test_module_data2 = std::make_shared<ModuleData>("test_module2");
   test_module_data2.get()->push(test_data); 
  
-  std::unique_ptr<BaseModule> test_module1(new TestModule("test_module1"));
-  std::unique_ptr<BaseModule> test_module2(new TestModule("test_module2"));
+  std::unique_ptr<BaseModule> test_module1(new TestModule("test_module1", "test_module1"));
+  std::unique_ptr<BaseModule> test_module2(new TestModule("test_module2", "test_module1"));
 
   ModuleManager manager;
   manager.AddModule(std::move(test_module1), test_module_data1);
