25a1e1234a5d3c26a101e33de9981811cb006bcd
kcaldwell <kcaldwell@nuro.ai>
2021-06-01 16:19:28 -0700

    updating module manager test

--- src/Modules/module_manager_test.cc
+++ src/Modules/module_manager_test.cc
@@ -1,18 +1,15 @@
 #include <gtest/gtest.h>
-
 #include <iostream>
-#include "module_manager.h"
-#include "base_module.h"
-#include "module_data_wrapper.h"
 #include <memory>
+#include "module_manager.h"
 #include "test_module.h"
 
 TEST(ModuleManagerTests, BasicFunctionality) {
 
   std::cout.flush();
 
-  std::unique_ptr<BaseModule> test_module1(new TestModule("test_module1", "test_module1"));
-  std::unique_ptr<BaseModule> test_module2(new TestModule("test_module2", "test_module1"));
+  std::unique_ptr<BaseModule> test_module1(new TestModule);
+  std::unique_ptr<BaseModule> test_module2(new TestModule);
 
   ModuleManager manager;
   manager.AddModule(std::move(test_module1));
